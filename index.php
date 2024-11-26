a360e97fc67e0f630adb9b2ba2150227559737b2
Maxence VILLET SCHOUMAKER <137928220+Maxence-villet@users.noreply.github.com>
2024-11-26 11:55:21 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button id="btn-rechercher">Rechercher</button>
-    <div id="resultat"></div>
-    <script src="scripts.js"></script>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <button id="btn-rechercher">Rechercher</button>
+        <div id="resultat"></div>
+        <script src="scripts.js"></script>
 
-    <form id="myForm">
-    <input type="text" id="dataToInsert" placeholder="Entrez votre donnée">
-    <button type="button" id="btn-recherche">Insérer</button>
-</form>
-</body>
+        <form id="myForm">
+            <input type="text" id="dataToInsert" placeholder="Entrez votre donnée">
+            <button type="button" id="btn-recherche">Insérer</button>
+        </form>
+    </body>
 </html>
